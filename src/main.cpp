b6da68070618cdcc56c3c3f06218004a630395f7
David Varshanidze <davidvarshanidze1@gmail.com>
2024-12-10 16:38:01 +0400

    add cmakelists and testing main

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include </opt/homebrew/Cellar/glfw/3.4/include/GLFW/glfw3.h>
+#include <iostream>
+
+// Error Callback
+void error_callback(int error, const char* description) {
+    std::cerr << "Error: " << description << std::endl;
+}
+
+int main() {
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Puzzle Game Engine", nullptr, nullptr);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
